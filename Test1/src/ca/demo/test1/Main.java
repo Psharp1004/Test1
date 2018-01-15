77b1ca60bcc7f6c16e9b9cd02fe99f7eb964746f
Peng <zhengp2@mcmaster.ca>
2018-01-14 21:25:25 -0500

    Initial checkin

---
+++ Test1/src/ca/demo/test1/Main.java
@@ -0,0 +1,16 @@
+package ca.demo.test1;
+
+public class Main{
+	public static void main(String[] args) {
+		Test1 data = new Test1();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello World!!!");
+		System.out.println("This app shows test data");
+	}
+}
+
---
+++ Test1/src/ca/demo/test1/Test1.java
@@ -0,0 +1,9 @@
+package ca.demo.test1;
+
+public class Test1 {
+	int data[]= {0,4,42};
+	
+	public void display() {
+		System.out.println("ToDo: Implement data display");
+	}
+}
